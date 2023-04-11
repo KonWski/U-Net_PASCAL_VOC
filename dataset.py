1740c4076cf7eb727437e8b326c05a98ec408ed8
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-04-11 22:38:15 +0200

    split_image_mask

--- dataset.py
+++ dataset.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 import torch
 from typing import List
+import math
+import torch.nn.functional as F
 
 class PascalVOCSegmentation(VOCSegmentation):
 
@@ -106,12 +108,113 @@ class PascalVOCSegmentation(VOCSegmentation):
 
         # additional channel for background
         encoded_mask = torch.zeros([mask.shape[0], mask.shape[1], len(self.selected_classes) + 1])
-
+        
+        # convert color encoding into channel encoding -
+        # - each channel refers to specific class
         for selected_class in self.selected_classes:
             
             channel_id = self.class_to_color[selected_class][0]
             class_color_encoding = self.class_to_color[selected_class][1]
             class_pixels_indices = np.where(mask == class_color_encoding)
-            encoded_mask[class_pixels_indices[0], class_pixels_indices[1], channel_id] = 1
+            encoded_mask[class_pixels_indices[0], class_pixels_indices[1], channel_id] = 1        
 
         return image, encoded_mask
+    
+
+# 572x572
+def split_image_mask(image: torch.Tensor, mask: torch.Tensor, splitted_mask_size: int, default_boundary: int):
+    '''
+    Converts image and mask into smaller pieces. Each of smaller pieces of image have the same shape.
+    The same rule goes for each smaller piece of mask.
+    
+    image: torch.Tensor
+        image split into smaller parts
+    mask: torch.Tensor
+        mask split into smaller parts
+    splitted_mask_size: int
+        width and height of smaller piece of mask
+    default_boundary: int:
+        padding size around cut out image piece
+    '''
+    
+    # temporarily change order of dimensions in mask
+    mask = mask.view(2, 0, 1)
+
+    # cut out parts of original image and mask
+    output_subimages = []
+    output_submasks = []
+    
+    image_height = image.shape[1]
+    image_width = image.shape[2]
+    
+    # n_rows x n_cols is the number of output smaller parts
+    n_rows = math.ceil(image_height / splitted_mask_size)
+    print(f"n_rows: {n_rows}")
+    n_cols = math.ceil(image_width / splitted_mask_size)
+    print(f"n_cols: {n_cols}")
+
+    for n_row in range(n_rows):
+        
+        # upper and lower border of mask/image piece
+        print(f"n_row: {n_row}")
+        row_split_0 = n_row * splitted_mask_size
+        print(f"row_split_0: {row_split_0}")
+        row_split_1 = (n_row + 1) * splitted_mask_size
+        print(f"row_split_1: {row_split_1}")
+        
+        # corner case - lower border exceeds original image
+        if row_split_1 > image_height:
+            padding_img_bottom = default_boundary + (row_split_1 - image_height)
+            padding_mask_bottom = row_split_1 - image_height
+        else:
+            padding_img_bottom = default_boundary
+            padding_mask_bottom = 0
+        
+        for n_column in range(n_cols):
+            
+            # left and right border of mask/image piece
+            print(f"n_column: {n_column}")
+            column_split_0 = n_column * splitted_mask_size
+            print(f"column_split_0: {column_split_0}")
+            column_split_1 = (n_column + 1) * splitted_mask_size
+            print(f"column_split_1: {column_split_1}")
+            
+            # corner case - right border exceeds original image
+            if column_split_1 > image_width:
+                padding_img_right = default_boundary + (column_split_1 - image_width)
+                padding_mask_right = column_split_1 - image_width
+            else:
+                padding_img_right = default_boundary
+                padding_mask_right = 0
+            
+            '''
+            padding size for image and mask pieces
+            padding size (left, right, top, bottom)
+            '''
+            padding_img = (default_boundary, padding_img_right, default_boundary, padding_img_bottom)
+            print(f"padding_img: {padding_img}")
+            padding_mask = (0, padding_mask_right, 0, padding_mask_bottom)
+            print(f"padding_mask: {padding_mask}")
+
+            # extract subimage and submask from input image and mask
+            sub_image = image[:, row_split_0:row_split_1, column_split_0:column_split_1]
+            sub_mask = mask[:, row_split_0:row_split_1, column_split_0:column_split_1]            
+
+            # return to original dimensions order in piece of mask
+            sub_mask = sub_mask.view(1, 2, 0)
+
+            print(f"sub_image before padding shape: {sub_image.shape}")
+            print(f"sub_mask before padding shape: {sub_mask.shape}")
+            
+            # add padding for image and mask piece
+            sub_image = F.pad(sub_image, padding_img)
+            sub_mask = F.pad(sub_mask, padding_mask)
+            
+            print(f"sub_image after padding shape: {sub_image.shape}")
+            print(f"sub_mask after padding shape: {sub_mask.shape}")            
+            
+            # collect next pieces of image and mask part
+            output_subimages.append(sub_image)
+            output_submasks.append(sub_mask)
+    
+    return output_subimages, output_submasks
---
+++ train.py
@@ -0,0 +1,127 @@
+from typing import List
+from dataset import PascalVOCSegmentation, split_image_mask
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss
+from torch.optim import Adam
+from model import uNetPascalVOC
+import logging
+from datetime import datetime
+
+def train_model(
+        device, 
+        n_epochs: int,
+        batch_size: int,
+        checkpoints_dir: str,
+        download_datasets: bool,
+        root_datasets_dir: str,
+        selected_classes: List[str]
+    ):
+    '''
+    n_epochs: int
+        number of training epochs
+    batch_size: int
+        number of images inside single batch
+    checkpoints_dir: str
+        path to directory where checkpoints will be stored
+    download_datasets: bool
+        True -> download dataset from torchvision repo
+    root_datasets_dir: str
+        path to directory where dataset should be downloaded (download_datasets = True)
+        or where dataset is already stored
+    selected_classes: List[str]
+        names of selected classes
+    '''
+
+    transform_train = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(p=0.5)
+    ])
+
+    # datasets and dataloaders
+    trainset = PascalVOCSegmentation(f'{root_datasets_dir}/train/', image_set="train", selected_classes=selected_classes, 
+                                     download=download_datasets, transform=transform_train)
+    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+    testset = PascalVOCSegmentation(f'{root_datasets_dir}/test/', image_set="test", selected_classes=selected_classes, 
+                                     download=download_datasets)
+    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+
+    n_classes = len(selected_classes)
+    len_trainset = len(trainset)
+    len_testset = len(testset)
+
+    # model
+    model = uNetPascalVOC(max_depth_level=4, n_classes=n_classes)
+    model = model.to(device)
+    optimizer = Adam(model.parameters(), lr=1e-5)
+
+    for epoch in range(n_epochs):
+        
+        checkpoint = {}
+
+        for state, loader, len_dataset in zip(["train", "test"], [train_loader, test_loader], [len_trainset, len_testset]):
+
+            # calculated parameters
+            running_loss = 0.0
+
+            if n_classes == 2:
+                criterion = BCEWithLogitsLoss()
+            else:
+                criterion = CrossEntropyLoss()
+
+            if state == "train":
+                model.train()
+            else:
+                model.eval()
+
+            for id, batch in enumerate(loader, 0):
+
+                with torch.set_grad_enabled(state == 'train'):
+                    
+                    images, masks = batch
+                    images, masks = split_image_mask(images, masks)
+
+                    images = images.to(device)
+                    masks = masks.to(device)
+                    optimizer.zero_grad()
+
+                    # calculate loss
+                    outputs = model(images).to(device)
+                    loss = criterion(outputs, masks)
+
+                    if state == "train":
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item()
+
+            # save and log epoch statistics
+            epoch_loss = round(running_loss / len_dataset, 2)
+
+            # save stats for potential checkpoint
+            checkpoint[f"{state}_loss"] = epoch_loss
+
+            logging.info(f"Epoch: {epoch}, state: {state}, loss: {epoch_loss}")
+
+        if checkpoint["test_loss"] < best_test_loss:
+            
+            # update lowest test loss
+            best_test_loss = checkpoint["test_loss"]
+
+            # save model to checkpoint
+            checkpoint["epoch"] = epoch
+            checkpoint["n_classes"] = n_classes
+            checkpoint["selected_classes"] = ",".join(n_classes)
+            checkpoint["model_state_dict"] = model.state_dict()
+            checkpoint["save_dttm"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            checkpoint_path = f"{checkpoints_dir}/uNetPascalVOC"
+            # save_checkpoint(checkpoint, checkpoint_path)
+
+        else:
+            logging.info(8*"-")
+            
+    return model

aee74337bb41d2bbc3fc6ebb3f8836ef3e2bfe94
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-05-11 19:01:02 +0200

    padding mask

--- dataset.py
+++ dataset.py
@@ -226,8 +226,11 @@ class PascalVOCSegmentation(VOCSegmentation):
                 sub_mask = mask[:, row_split_mask_0:row_split_mask_1, column_split_mask_0:column_split_mask_1]            
                 
                 # set part of mask that was extended by padding as background
-                sub_mask[0, :, -padding_mask_right:] = torch.ones([sub_mask.shape[1], padding_mask_right])
-                sub_mask[0, -padding_mask_bottom:, :] = torch.ones([padding_mask_bottom, sub_mask.shape[2]])
+                if padding_mask_right > 0:
+                    sub_mask[0, :, -padding_mask_right:] = torch.ones([sub_mask.shape[1], padding_mask_right])
+                
+                if padding_mask_bottom > 0:
+                    sub_mask[0, -padding_mask_bottom:, :] = torch.ones([padding_mask_bottom, sub_mask.shape[2]])
 
                 # return to original dimensions order in piece of mask
                 # sub_mask = sub_mask.permute(1, 2, 0)

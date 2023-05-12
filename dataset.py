51c5893bdfbff938809aad8603b94f5f36d5f2b9
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-05-12 22:48:29 +0200

    _split_image_mask

--- dataset.py
+++ dataset.py
@@ -159,8 +159,8 @@ class PascalVOCSegmentation(VOCSegmentation):
             # print(f"n_row: {n_row}")
             
             row_split_mask_0 = n_row * self.splitted_mask_size
-            # row_split_img_0 = max(0, row_split_mask_0 - self.default_boundary)
-            row_split_img_0 = row_split_mask_0
+            row_split_img_0 = max(0, row_split_mask_0 - self.default_boundary)
+            # row_split_img_0 = row_split_mask_0
 
             row_split_mask_1 = (n_row + 1) * self.splitted_mask_size
             # print(f"row_split_1: {row_split_1}")
@@ -168,7 +168,9 @@ class PascalVOCSegmentation(VOCSegmentation):
             row_split_img_1 = min(image_height, row_split_mask_1 + self.default_boundary)
             # print(f"row_split_image_1: {row_split_img_1}")
 
+            # TODO fix - gives negative values
             padding_img_top = abs(row_split_mask_0 - self.default_boundary) - row_split_img_0
+
             padding_img_bottom = (row_split_mask_1 + self.default_boundary) - row_split_img_1
             # print(f"padding_img_top: {padding_img_top}")
             # print(f"padding_img_bottom: {padding_img_bottom}")
@@ -185,13 +187,15 @@ class PascalVOCSegmentation(VOCSegmentation):
                 
                 # left and right border of mask/image piece
                 column_split_mask_0 = n_column * self.splitted_mask_size
-                # column_split_img_0 = max(0, column_split_mask_0 - self.default_boundary)
-                column_split_img_0 = column_split_mask_0
+                column_split_img_0 = max(0, column_split_mask_0 - self.default_boundary)
+                # column_split_img_0 = column_split_mask_0
 
                 column_split_mask_1 = (n_column + 1) * self.splitted_mask_size
                 column_split_img_1 = min(image_width, column_split_mask_1 + self.default_boundary)
-
+                
+                # TODO fix - gives negative values
                 padding_img_left = abs(column_split_mask_0 - self.default_boundary) - column_split_img_0
+
                 padding_img_right = (column_split_mask_1 + self.default_boundary) - column_split_img_1
 
                 # corner case - right border exceeds original image
@@ -217,8 +221,8 @@ class PascalVOCSegmentation(VOCSegmentation):
                 sub_image = image[:, row_split_img_0:row_split_img_1, column_split_img_0:column_split_img_1]
                 sub_mask = mask[:, row_split_mask_0:row_split_mask_1, column_split_mask_0:column_split_mask_1]            
 
-                print(f"Image, with: {row_split_img_0}:{row_split_img_1}, height: {column_split_img_0}:{column_split_img_1}")
-                print(f"Mask, with: {row_split_mask_0}:{row_split_mask_1}, height: {column_split_mask_0}:{column_split_mask_1}")
+                print(f"Image, height: {row_split_img_0}:{row_split_img_1}, width: {column_split_img_0}:{column_split_img_1}")
+                print(f"Mask, height: {row_split_mask_0}:{row_split_mask_1}, width: {column_split_mask_0}:{column_split_mask_1}")
 
                 # return to original dimensions order in piece of mask
                 # sub_mask = sub_mask.permute(1, 2, 0)

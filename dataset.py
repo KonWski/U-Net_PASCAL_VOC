518da719c70ed9dd00455257a528db8927f0345f
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-05-30 23:04:28 +0200

    augmentation rotation

--- dataset.py
+++ dataset.py
@@ -165,10 +165,16 @@ class PascalVOCSegmentation(VOCSegmentation):
             mask = center_crop(mask, [285, 285])
 
         # rotation
-        # if random.random() > 0.5:
-        #     angle = random.randint(1, 30)
-        #     image = rotate(image, angle)
-        #     mask = rotate(mask, angle, fill=0)
+        if random.random() > 0.5:
+
+            angle = random.randint(1, 30)
+            image = rotate(image, angle, fill=0)
+            mask = rotate(mask, angle, fill=0)
+
+            # fill new mask values with background class
+            misssing_background_indices = np.all(mask.numpy() == [0, 0, 0], -1)
+            misssing_background_indices = torch.Tensor(np.where(misssing_background_indices, 1, 0))
+            mask[:, :, 0] = misssing_background_indices
 
         # color jitter
         if random.random() > 0.5:

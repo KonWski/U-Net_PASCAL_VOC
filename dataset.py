c5eb219528e87041c3c6d14596a3ba4f20d79c4f
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-06-03 22:20:39 +0200

    __getitem__

--- dataset.py
+++ dataset.py
@@ -323,11 +323,11 @@ class PascalVOCSegmentation(VOCSegmentation):
                 encoded_mask[:, :, channel_id] = class_pixels_indices
 
         # fill uncovered mask parts (when only part of classes were used) with background
-        misssing_background_indices = np.all(encoded_mask.numpy() == [0, 0, 0], -1)
+        misssing_background_indices = np.all(encoded_mask.numpy() == [0 for selected_class in len(self.selected_classes)], -1)
         misssing_background_indices = torch.Tensor(np.where(misssing_background_indices, 1, 0))
 
         if torch.numel(misssing_background_indices) > 0:
-            encoded_mask[:, :, 0] = misssing_background_indices
+            encoded_mask[:, :, 0] = encoded_mask[:, :, 0] + misssing_background_indices
 
         if self.augmentation:
             image, encoded_mask = self._transform(image, encoded_mask)

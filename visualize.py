49079992ec2848c5ac35cc9ad7827725e6069fea
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-09-04 23:22:15 +0200

    cleanup

--- visualize.py
+++ visualize.py
@@ -13,8 +13,6 @@ def concat_split_mask(split_mask: List[torch.Tensor], original_image: torch.Tens
     n_selected_classes: int
         number of classes to which mask refer
     '''
-    print(f"original_image.shape: {original_image.shape}")
-    print(f"split_mask[0].shape: {split_mask[0].shape}")
 
     # all pieces of mask have the same size
     height_mask_split = split_mask[0].shape[1]
@@ -22,16 +20,11 @@ def concat_split_mask(split_mask: List[torch.Tensor], original_image: torch.Tens
 
     original_image_height = original_image.shape[2]
     original_image_width = original_image.shape[3]
-    print(f"original_image_height: {original_image_height}")
-    print(f"original_image_width: {original_image_width}")
 
     mask = torch.zeros([n_selected_classes, original_image_height, original_image_width])
-    print(f"mask.shape: {mask.shape}")
 
     n_rows = ceil(original_image_height / height_mask_split)
     n_cols = ceil(original_image_width / width_mask_split)
-    print(f"n_rows: {n_rows}")
-    print(f"n_cols: {n_cols}")
 
     mask_split_iterator = 0
 
@@ -46,12 +39,8 @@ def concat_split_mask(split_mask: List[torch.Tensor], original_image: torch.Tens
             width_index_0 = n_col * width_mask_split
             width_index_1 = min((n_col + 1) * width_mask_split, original_image_width)
 
-            try:
-                cut_mask_piece = mask_piece[:, 0 : height_index_1 - height_index_0, 0 : width_index_1 - width_index_0]
-                mask[:, height_index_0 : height_index_1, width_index_0 : width_index_1] = cut_mask_piece
-                mask_split_iterator += 1
-            except:
-                print(f"Height indices -> {height_index_0}:{height_index_1}, Width indices -> {width_index_0}:{width_index_1}")
-                print(f"mask piece shape: {mask_piece[:, 0 : height_index_1 - height_index_0, 0 : width_index_1 - width_index_0].shape}")
+            cut_mask_piece = mask_piece[:, 0 : height_index_1 - height_index_0, 0 : width_index_1 - width_index_0]
+            mask[:, height_index_0 : height_index_1, width_index_0 : width_index_1] = cut_mask_piece
+            mask_split_iterator += 1
 
     return mask

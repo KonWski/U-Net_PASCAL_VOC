475156ce18d7cb1f6a573a80d627e7a975280b80
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-09-07 23:06:22 +0200

    get_class_weights

--- auxiliary.py
+++ auxiliary.py
@@ -23,7 +23,7 @@ def get_class_weights(dataset: PascalVOCSegmentation):
             print(f"mask_piece.shape: {mask_piece.shape}")
             # calculate number of pixels with given class
             for n_class in range(n_selected_classes):
-                class_occurences[n_class] = class_occurences[n_class] + mask_piece[n_class, :, :].sum().item()
+                class_occurences[n_class] = class_occurences[n_class] + mask_piece[0][n_class, :, :].sum().item()
 
     # find class with most occurences
     most_common_class = 0
